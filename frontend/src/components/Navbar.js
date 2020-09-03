39474e3d5e0be8cce8736e29894a25188780cffd
Jos Bogan <Josbogan@gmail.com>
2020-09-03 14:37:25 +0100

    trying authentication navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
 
-function Navbar() {
+// Libraries
+import React, {useEffect} from 'react'
+import { Link, withRouter } from 'react-router-dom'
+
+// Lib
+import Auth from '../lib/auth'
+
+function Navbar(props) {
+
+  useEffect(() => {
+    console.log('here')
+  }, [])
+
   return (
     <nav className="navbar">
       <Link to="/" className="navbar_item">Logo</Link>
       <div className="navbar_item navbar_right navbar_auth">
         <Link to="/rooms" className="navbar_item navbar_auth_item">Rooms</Link>
-        <Link to="/login" className="navbar_item navbar_auth_item">Login</Link>
-        <Link to="/register" className="navbar_item navbar_auth_item">Register</Link>
+        {!Auth.isAuthenticated() && <Link to="/login" className="navbar_item navbar_auth_item">Login</Link>}
+        {!Auth.isAuthenticated() && <Link to="/register" className="navbar_item navbar_auth_item">Register</Link>}
       </div>
     </nav>
   )
 }
 
-export default Navbar
+export default withRouter(Navbar)
